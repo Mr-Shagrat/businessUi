f56789a82978c6fc3d1a653c832b7b089100077a
Mr-Shagrat <cobhc900@gmail.com>
2021-08-03 23:04:14 +0300

    second site (html, css, js)

---
+++ js/script.js
@@ -0,0 +1,49 @@
+'use strict'
+const iconMenu = document.querySelector('.menu-header__icon');
+
+if (iconMenu) {
+  const menuBody = document.querySelector('.menu-header__menu');
+
+  iconMenu.addEventListener("click", function (e) {
+
+    document.body.classList.toggle('lock');
+    iconMenu.classList.toggle('active');
+    menuBody.classList.toggle('active');
+
+  });
+}
+//Задаем фон блоку из картинки
+function ibg() {
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+ibg();
+
+
+const tabsLink = document.querySelectorAll('.tabs__item')
+const tabsBlock = document.querySelectorAll('.tabs__block')
+
+
+for (let i = 0; i < tabsLink.length; i++) {
+
+  tabsLink[i].addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const activeTabLink = document.querySelector('.tabs__item.active')
+    const activeTabBlock = document.querySelector('.tabs__block.active')
+    const tabsId = e.target.getAttribute('href').replace('#', '')
+
+    activeTabLink.classList.remove('active')
+    activeTabBlock.classList.remove('active')
+    tabsLink[i].classList.add('active')
+
+
+    document.querySelector(`#${tabsId}`).classList.add('active')
+  })
+}
+
+
